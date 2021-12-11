d5c543d56c6f7cac215621937f0e4f8617307a2a
Ernest Asanov <ern.asanov@yandex.ru>
2021-12-11 13:46:07 +0300

    coroutines example

--- RecyclerViewSample/app/src/main/java/me/shafran/rvsample/PersonListActivity.kt
+++ RecyclerViewSample/app/src/main/java/me/shafran/rvsample/PersonListActivity.kt
@@ -1,36 +1,84 @@
 package me.shafran.rvsample
 
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.*
+import java.lang.ref.WeakReference
+import kotlin.math.roundToInt
 
 class PersonListActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_person_list)
+	//private lateinit var task: LoadTask
 
-		val recyclerView: RecyclerView = findViewById(R.id.personRecyclerView)
-		recyclerView.layoutManager = LinearLayoutManager(this)
-		recyclerView.setHasFixedSize(true)
-		recyclerView.recycledViewPool.setMaxRecycledViews(0, 5)
+    private lateinit var coroutine: Job
 
-		val adapter = PersonAdapter()
-		recyclerView.adapter = adapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person_list)
 
-		val thread = Thread {
-			PersonRepository.initialize(this)
-			adapter.personList = PersonRepository.getPersonList()
-			recyclerView.post {
-				adapter.notifyDataSetChanged()
-			}
-		}
-		thread.start()
-		adapter.listener = { personId ->
-			val intent = PersonDetailActivity.getIntent(this, personId)
-			startActivity(intent)
-		}
+        val recyclerView: RecyclerView = findViewById(R.id.personRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.recycledViewPool.setMaxRecycledViews(0, 5)
+
+        val adapter = PersonAdapter()
+        recyclerView.adapter = adapter
+
+        val progressView = findViewById<TextView>(R.id.progressView)
+
+		/*task = LoadTask(WeakReference(this), { progress ->
+			progressView.text = "${(progress * 100).roundToInt()}%"
+		}, { list ->
+			adapter.personList = list
+			adapter.notifyDataSetChanged()
+		})
+		task.execute()*/
+        lifecycleScope.launch(context = Dispatchers.Main) {
+            progressView.text = "0%"
+            adapter.personList =
+                PersonRepository.initialize(this@PersonListActivity) { progress ->
+                    progressView.text = "${(progress * 100).roundToInt()}%"
+                }
+            adapter.notifyDataSetChanged()
+        }
+        adapter.listener = { personId ->
+            val intent = PersonDetailActivity.getIntent(this, personId)
+            startActivity(intent)
+        }
+    }
+
+	override fun onDestroy() {
+		//task.cancel(true)
+		super.onDestroy()
 	}
+
+	/*class LoadTask(
+        private val context: WeakReference<Context>,
+        private val onProgress: (Float) -> Unit,
+        private val onComplete: (List<Person>) -> Unit
+    ) : AsyncTask<Void, Float, List<Person>>() {
+        override fun doInBackground(vararg params: Void?): List<Person> {
+            PersonRepository.initialize(context.get() ?: return emptyList()) { progress ->
+                this.publishProgress(progress * 100)
+            }
+            return PersonRepository.getPersonList()
+        }
+
+		override fun onProgressUpdate(vararg values: Float?) {
+			super.onProgressUpdate(*values)
+			values[0]?.let { onProgress(it) }
+		}
+
+		override fun onPostExecute(result: List<Person>?) {
+			super.onPostExecute(result)
+			result?.let { onComplete(it) }
+		}
+    }*/
 }
--- RecyclerViewSample/app/src/main/java/me/shafran/rvsample/PersonRepository.kt
+++ RecyclerViewSample/app/src/main/java/me/shafran/rvsample/PersonRepository.kt
@@ -2,14 +2,37 @@ package me.shafran.rvsample
 
 import android.content.Context
 import android.text.TextUtils
+import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.coroutines.suspendCoroutine
 
 object PersonRepository {
 	private val PERSON_LIST = mutableListOf<Person>()
-	fun initialize(context: Context) {
+	private fun loadSomethingBlocking(): Int {
+		return runBlocking { loadSomething() }
+	}
+	private fun loadSomething(onLoaded: (Int) -> Unit, onFailed: (Exception) -> Unit) {
+		Thread {
+			try {
+				onLoaded(loadSomethingBlocking())
+			} catch (e: Exception) {
+				onFailed(e)
+			}
+		}.start()
+	}
+	suspend fun loadSomething(): Int {
+		return suspendCoroutine { continuation ->
+			loadSomething({
+				continuation.resumeWith(Result.success(it))
+			}, {
+				continuation.resumeWith(Result.failure(it))
+			})
+		}
+	}
+	suspend fun initialize(context: Context, onProgress: suspend (Float) -> Unit): List<Person> = withContext(Dispatchers.IO) {
 		try {
 			BufferedReader(InputStreamReader(context.assets.open("names.txt"))).use { reader ->
 				var name = reader.readLine()
@@ -17,6 +40,14 @@ object PersonRepository {
 				while (!TextUtils.isEmpty(name)) {
 					PERSON_LIST.add(Person(id, name))
 					++id
+					if (id.toInt() % 30 == 0) {
+						launch {
+							withContext(Dispatchers.Main) {
+								onProgress(id / 346.0f)
+							}
+						}
+						delay(1000)
+					}
 					name = reader.readLine()
 				}
 			}
@@ -24,6 +55,7 @@ object PersonRepository {
 			e.printStackTrace()
 			// Ничего не делать
 		}
+		return@withContext PERSON_LIST.toList()
 	}
 
 	fun getPersonList(): List<Person> {
